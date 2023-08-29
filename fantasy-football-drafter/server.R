9754f8ce878393857ab99aa9f2120a964ed667e7
erichrubio <erichrubiooo@gmail.com>
2023-08-29 17:35:07 -0600

    Add button-dropped players to dropped players selectizeInput.

--- fantasy-football-drafter/server.R
+++ fantasy-football-drafter/server.R
@@ -1,7 +1,7 @@
-function(input, output) {
+function(input, output, session) {
 
   # Reactive dropped player IDs.
-  r.v.dropped <- reactiveValues(id = c())
+  r.v.button.dropped <- reactiveValues(id = c())
 
   # Reactive player projections.
   r.dt.pros <- reactiveValues(data = g.r.dt.pros)
@@ -9,7 +9,7 @@ function(input, output) {
   # Player projections data table output.
   output$dto_players <- DT::renderDataTable(
     r.dt.pros$data[
-      (!id %in% r.v.dropped$id) &
+      (!id %in% r.v.button.dropped$id) &
       (!Player %in% input$input_drop_players) &
       (Pos %in% input$input_position)
     ][order(Rank)][, !"id"],
@@ -21,7 +21,20 @@ function(input, output) {
   # Listens for "Drop" button click.
   observeEvent(input$select_button, {
     selectedRow <- as.numeric(strsplit(input$select_button, "_")[[1]][2])
-    r.v.dropped$id <- append(r.v.dropped$id, r.dt.pros$data[selectedRow, "id"])
+    r.v.button.dropped$id <- unique(
+      append(
+        r.v.button.dropped$id,
+        r.dt.pros$data[selectedRow, "id"]
+      )
+    )
+    updateSelectizeInput(
+      session,
+      inputId = 'input_drop_players',
+      selected = g.r.dt.pros[
+        (id %in% r.v.button.dropped$id) |
+        (Player %in% input$input_drop_players)
+      ]$Player
+    )
   })
 
 }
